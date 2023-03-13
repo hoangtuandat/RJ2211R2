180a4435d11dcbf83daa15534041702ab318291c
Hoàng Tuấn Đạt <tuandathoang.ruler@gmail.com>
2023-03-13 20:27:07 +0700

    comboBox-country

--- casestudy2/src/ComboBox.js
+++ casestudy2/src/ComboBox.js
@@ -1,22 +1,71 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import React, { Component } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-export default function ComboBox() {
-  return (
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={top100Films}
-      sx={{ width: 200 }}
-      renderInput={(params) => <TextField {...params} label="Phòng ban" />}
-    />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const columns = [
+      { field: "Country", headerName: "Country", width: 150 },
+      { field: "Slug", headerName: "Slug", width: 150 },
+      { field: "ISO2", headerName: "ISO2", width: 150 },
+    ];
+    this.state = {columns: columns, rows: [], selectedCountry:''};
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch("https://api.covid19api.com/countries")
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          // console.log("result", data);
+          let id = 1;
+          const dataWithId = data.map((x) =>
+            Object.assign({}, x, { id: id++ })
+          );
+          // console.log("dataWithId", dataWithId);
+          this.setState({ rows: dataWithId });
+          this.props.totalCountries(dataWithId.length)
+        },
+        (error) => {
+          console.log("error", error);
+        }
+      );
+  };
+
+  handleChange = (event) => {
+    console.log("chọn", event.target.value);
+    this.setState({ selectedCountry: event.target.value });
+    this.props.handleChange(event.target.value);
+  };
+  render() {
+    return (
+      <div>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-select-small" style={{color:'white'}}>Countries</InputLabel>
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={this.state.selectedCountry}
+            onChange={this.handleChange}
+          >
+            {
+              this.state.rows.map((value, index) => {
+                return (
+                  <MenuItem key={index} value={value.Country}>{value.Country}</MenuItem>
+                )
+              })
+            }
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
 }
 
-
-const top100Films = [
-  { label: 'The Shawshank ', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  
-];
+export default App;
