be6c896b1ef58a7963b1c7afbcd70fbb0105be08
Hoàng Tuấn Đạt <tuandathoang.ruler@gmail.com>
2023-03-15 18:31:07 +0700

    add edit with form dialog

--- casestudy2/src/MyClass.js
+++ casestudy2/src/MyClass.js
@@ -9,11 +9,12 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 
 import Snackbar from "@mui/material/Snackbar";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
 
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 
 class MyClass extends Component {
   constructor(props) {
@@ -35,19 +36,21 @@ class MyClass extends Component {
           </div>
         ),
       },
-      { field: "picture", headerName: "Ảnh", width: 150,
-      renderCell: (params) => (
-        <div>
-          <Avatar alt="Remy Sharp" src={params.value} />
-        </div>
-      ),
-    },
+      {
+        field: "picture",
+        headerName: "Ảnh",
+        width: 150,
+        renderCell: (params) => (
+          <div>
+            <Avatar alt="Remy Sharp" src={params.value} />
+          </div>
+        ),
+      },
       { field: "firstName", headerName: "Tên", width: 150 },
       { field: "lastName", headerName: "Họ", width: 150 },
       { field: "country", headerName: "Quốc gia", width: 150 },
       { field: "phone", headerName: "Điện thoại", width: 150 },
       { field: "dob", headerName: "Ngày sinh", width: 150 },
-      
     ];
 
     this.state = {
@@ -56,10 +59,12 @@ class MyClass extends Component {
       selectedClass: props.selectedClass,
       openConfirmation: false,
       editStudent: null,
+      editedStudent: null,
       totalStudents: 0,
       maxID: 1,
       openSnackBar: false,
       snackBarInfo: "",
+      openEditer: false,
     };
   }
 
@@ -112,16 +117,27 @@ class MyClass extends Component {
 
   editRow = (id) => {
     // console.log("editRow", id);
+    const editStudent = this.state.students.find(
+      (students) => students.id === id
+    );
+    console.log("deletetRow", id, editStudent);
+    if (editStudent) {
+      this.setState({
+        openEditer: true,
+        editStudent: editStudent,
+        editedStudent: editStudent,
+      });
+    }
   };
+
   deletetRow = (id) => {
     const editStudent = this.state.students.find(
       (students) => students.id === id
-    )
+    );
     // console.log("deletetRow", id, editStudent);
-    if(editStudent) {
+    if (editStudent) {
       this.setState({ openConfirmation: true, editStudent: id });
     }
-    
   };
 
   handleCloseConfirmation = (yes) => {
@@ -140,8 +156,71 @@ class MyClass extends Component {
     }
   };
 
+  handleCloseEditer = (yes) => {
+    // console.log("handleCloseEditer", yes);
+    this.setState({ openEditer: false });
+    if (yes) {
+      // console.log("handleCloseEditer", this.state.editedStudent);
+      // let dialogStudent = this.state.students;
+      // dialogStudent = dialogStudent.filter(
+      //   (data) => data.id !== this.state.editStudent
+      // );
+      // const totalStudents = this.state.totalStudents - 1;
+      // // console.log('dialog',this.state.editStudent)
+      // this.setState({ students: dialogStudent, totalStudents: totalStudents });
+      // this.props.handleTotalStudents(totalStudents);
+      // this.setState({ openSnackBar: true, snackBarInfo: "Xoá thành công!" });
+    }
+  };
+
   handleSnackBarClose = () => {
     this.setState({ openSnackBar: false });
+  };
+
+  setFirstName = (event) => {
+    // console.log("setFirstName", event.target.value);
+    const editedStudent = this.state.editedStudent;
+    editedStudent.firstName = event.target.value;
+    this.setState({
+      editedStudent
+    })
+  };
+
+  setLastName = (event) => {
+    // console.log("setLastName", event.target.value);
+    const editedStudent = this.state.editedStudent;
+    editedStudent.lastName = event.target.value;
+    this.setState({
+      editedStudent
+    })
+
+  };
+  setCountry = (event) => {
+    // console.log("setLastName", event.target.value);
+    const editedStudent = this.state.editedStudent;
+    editedStudent.country = event.target.value;
+    this.setState({
+      editedStudent
+    })
+
+  };
+  setPhone = (event) => {
+    // console.log("setLastName", event.target.value);
+    const editedStudent = this.state.editedStudent;
+    editedStudent.phone = event.target.value;
+    this.setState({
+      editedStudent
+    })
+
+  };
+  setDob = (event) => {
+    // console.log("setLastName", event.target.value);
+    const editedStudent = this.state.editedStudent;
+    editedStudent.dob = event.target.value;
+    this.setState({
+      editedStudent
+    })
+
   };
 
   render() {
@@ -154,7 +233,6 @@ class MyClass extends Component {
     return (
       <div style={{ height: 600, width: "100%" }}>
         <DataGrid rows={displayStudents} columns={this.state.columns} />
-
 
         <Dialog
           open={this.state.openConfirmation}
@@ -181,18 +259,85 @@ class MyClass extends Component {
           </DialogActions>
         </Dialog>
 
+        <Dialog
+          open={this.state.openEditer}
+          onClose={this.state.handleCloseEditer}
+        >
+          <DialogTitle>Sửa thông tin sinh viên</DialogTitle>
+          <DialogContent>
+            <DialogContentText></DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="firstName"
+              onChange={this.setFirstName}
+              label="Tên"
+              type="text"
+              defaultValue={this.state.editStudent?.firstName}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="lasttName"
+              onChange={this.setLastName}
+              label="Họ"
+              type="text"
+              defaultValue={this.state.editStudent?.lastName}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="country"
+              onChange={this.setCountry}
+              label="Quốc gia"
+              type="text"
+              defaultValue={this.state.editStudent?.country}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="phone"
+              onChange={this.setPhone}
+              label="Điện thoại"
+              type="text"
+              defaultValue={this.state.editStudent?.phone}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="dob"
+              onChange={this.setDob}
+              label="Ngày sinh"
+              type="text"
+              defaultValue={this.state.editStudent?.dob}
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleCloseEditer(false)}>Huỷ</Button>
+            <Button onClick={() => this.handleCloseEditer(true)}>Lưu</Button>
+          </DialogActions>
+        </Dialog>
 
-          <Snackbar
-            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-            open={this.state.openSnackBar}
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.openSnackBar}
+          onClose={this.handleSnackBarClose}
+          message={this.state.snackBarInfo}
+          key={{ vertical: "bottom", horizontal: "left" }}
+        >
+          <Alert
             onClose={this.handleSnackBarClose}
-            message={this.state.snackBarInfo}
-            key={{ vertical: "bottom", horizontal: "left" }}
+            severity="info"
+            sx={{ width: "100%" }}
           >
-            <Alert onClose={this.handleSnackBarClose} severity="info" sx={{ width: '100%' }}>{this.state.snackBarInfo}
-  </Alert>
-          </Snackbar>
-          
+            {this.state.snackBarInfo}
+          </Alert>
+        </Snackbar>
       </div>
     );
   }
